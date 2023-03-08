9e6db0dc07974c8a8ec9d35ae7f579b8d629c3d5
BojanaRatkovic <111780122+BojanaRatkovic@users.noreply.github.com>
2023-03-08 19:39:41 +0100

    testpage

---
+++ Webpage/login.js
@@ -0,0 +1,32 @@
+document.getElementById("login").addEventListener("click", function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const myDiv = document.getElementById("form");
+  const myLoadingText = document.getElementById("Loading");
+  myDiv.style.display = "none";
+  myLoadingText.style.display = "block";
+
+  const loginData = { email: email, password: password };
+
+  fetch("https://js-course-server.onrender.com/user/login", {
+      method: "POST",
+      body: JSON.stringify(loginData),
+      headers: {
+          "Content-Type": "application/json",
+      },
+  })
+  .then((response) => response.json())
+  .then((data) => {
+      if (data.UserId) {
+          localStorage.setItem("auth_token", data.token);
+          localStorage.setItem("userId", data.UserId);
+          alert("Prijava uspešna!");
+          window.location.href = "testpage.html"; // redirect to testpage.html
+      } else {
+          alert("Neuspešno");
+          myDiv.style.display = "block";
+          myLoadingText.style.display = "none";
+      }
+  })
+});
